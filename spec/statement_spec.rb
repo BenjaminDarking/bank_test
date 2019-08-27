b7ce5f89f7ffe63412eed14fe3e2bb8335cf32ec
Ben D <benjamin.d928@icloud.com>
2019-08-27 11:54:01 +0100

    Time recorded when a withdrawl is made

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -26,4 +26,9 @@ describe Account do
         expect(account.balance).to eq 0
         expect(account.statement).to_not be_empty
     end
+    it "records time of withdrawl" do 
+        account = Account.new
+        account.withdraw(100)
+        expect(account.statement[0].time.to_s).to eq(Time.now.to_s)
+    end
 end
